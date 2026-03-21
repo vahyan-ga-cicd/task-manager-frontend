bcd110220cde370afd6ddf490800d4fc404006fd
Dipayan Dey <deydipayan2006@gmail.com>
2026-03-21 12:37:37 +0530

    feat : creat invoice cross button bg remove

--- src/components/dashboard/admin/invoice/account/CreateInvoiceModal.tsx
+++ src/components/dashboard/admin/invoice/account/CreateInvoiceModal.tsx
@@ -98,7 +98,7 @@ const CreateInvoiceModal: React.FC<CreateInvoiceModalProps> = ({
           </h3>
           <button
             onClick={onClose}
-            className="p-2 text-slate-400 hover:text-slate-600 hover:bg-slate-100 rounded-xl transition-all bg-gray-400"
+            className="p-2 text-slate-400 hover:text-slate-600 hover:bg-slate-100 rounded-xl transition-all "
           >
             <X className="w-6 h-6" />
           </button>
